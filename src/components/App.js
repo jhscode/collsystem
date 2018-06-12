8b3c1da2b5582d0d61e744721ba6bdc77768094f
James Shin <jhscode@gmail.com>
2018-06-12 08:15:52 -0400

    updated app with components

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import Navigation from './Navigation';
 import Summary from './Summary';
+import Counterparties from './Counterparties';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Summary />
+        <header>
+          <Navigation />
+        </header>
+        <div>
+          <Summary />
+        </div>
+        <Counterparties />
       </div>
     );
   }
